437d0da92599efa7ea8fdb7741b58a8afc755174
almas.adilbekov555@gmail.com <almas.adilbekov555@gmail.com>
2020-12-28 21:26:05 +0600

    initial commit

---
+++ qwerty/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from qwerty import models 
+admin.site.register(models.Person)
+admin.site.register(models.Bank)
+admin.site.register(models.Lombard)
+# Register your models here.
---
+++ qwerty/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from qwerty import models
+
+
+def first_page(self):
+    persons = models.Person.objects.all()
+    data = {
+            "asd": persons
+        }
+    return render(self, 'index.html', data)
+
+
+def second_page(self):
+    persons = models.Bank.objects.all()
+    data = {
+            "qwe": persons
+        }
+    return render(self, 'index1.html',data)
+
+
+
+def second2_page(self):
+    persons = models.Lombard.objects.all()
+    data = {
+        "zxc": persons
+    }
+    return render(self, 'index2.html',data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def third_page(self):
+    return render(self, 'index3.html')
+
+
+def fourth_page(self):
+    return render(self, 'index4.html')
